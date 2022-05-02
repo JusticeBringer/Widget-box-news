a1413d5e70211cfd6c52be256e97e3a5dc316585
justicebringer <gabithebigg@gmail.com>
2022-05-02 17:16:06 +0300

    Change http to https in url

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ window.onload = async function () {
     return;
   }
 
-  await fetch("http://www.mocky.io/v2/58fda6ce0f0000c40908b8c8")
+  await fetch("https://www.mocky.io/v2/58fda6ce0f0000c40908b8c8")
     .then((response) => response.json())
     .then((data) => {
       GLOBAL_NEWS = data.news;
